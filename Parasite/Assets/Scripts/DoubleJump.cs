cdaa9dc530c48d608eef1f993afbf752280fe550
JVersaggi <jacver77@yahoo.com>
2019-11-09 11:30:13 -0500

    fjdjtdf

--- Parasite/Assets/Scripts/DoubleJump.cs
+++ Parasite/Assets/Scripts/DoubleJump.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class DoubleJump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int maxjumps = 2;
+    int jumps;
+    float jumpforce = 5f;
+    bool grounded;
+    public float movespeed;
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveL();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveR();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+    }
+
+    void MoveL()
+    {
+
+    }
+
+    void MoveR()
+    {
+
+    }
+
+    void Jump()
+    {
+        if (jumps > 0)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector2(0, jumpforce), ForceMode.Impulse);
+            grounded = false;
+            jumps = jumps - 1;
+        }
+        if (jumps == 0)
+        {
+            return;
+        }
+    }
+    void OnCollisionEnter(Collision collide)
+    {
+        if (collide.gameObject.tag == "Ground")
+        {
+            jumps = maxjumps;
+            grounded = true;
+            movespeed = 2.0F;
+        }
     }
 }
---
+++ Parasite/Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    int nrOfAlowedDJumps = 1;
+    int dJumpCounter = 0;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        {
+            moveDirection.x = Input.GetAxis("Horizontal") * speed;
+            moveDirection.z = Input.GetAxis("Vertical") * speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (characterController.isGrounded)
+                {
+                    moveDirection.y = jumpSpeed;
+                    dJumpCounter = 0;
+                }
+                if (!characterController.isGrounded && dJumpCounter < nrOfAlowedDJumps)
+                {
+                    moveDirection.y = jumpSpeed;
+                    dJumpCounter++;
+                }
+            }
+            moveDirection.y -= gravity * Time.deltaTime;
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+    }
+}
